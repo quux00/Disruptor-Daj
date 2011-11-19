774ccbef77ea6027f0f91565af681678a81b8d88
Michael <midpeter444@gmail.com>
2011-11-19 13:14:24 -0500

    Completed ThreePublisherDemo
    I want to try to do a Multi-publisher scenario with the Disruptor Fluent API next

--- src/demo/disruptor/ThreePublishersDemo.java
+++ src/demo/disruptor/ThreePublishersDemo.java
@@ -1,59 +1,122 @@
 package demo.disruptor;
 
+import java.util.*;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 import com.lmax.disruptor.*;
-import demo.disruptor.util.SingleSlotProcessor;
+import demo.disruptor.util.DemoEventHandler;
 
 public class ThreePublishersDemo {
   
   public final static int RING_BUFFER_SIZE = 8;
+  public final static int NUM_MSGS_TO_PUBLISH = 18;  // needs to be evenly divisible by 3
+  public final static int NUM_PUBLISHERS = 3;
   final RingBuffer<DemoEvent> ringBuf;
   final BatchEventProcessor<DemoEvent> batchProc;
+  final CountDownLatch latch = new CountDownLatch(NUM_MSGS_TO_PUBLISH); // latch to wait until the consumer is finished
+  final CyclicBarrier cyclicBarrier = new CyclicBarrier(NUM_PUBLISHERS);
+  final List<DemoPublisher> pubs;   // list of 3 publishers 
 
   public ThreePublishersDemo() {
     ringBuf = new RingBuffer<DemoEvent>(DemoEvent.FACTORY,
                                         new MultiThreadedClaimStrategy(RING_BUFFER_SIZE),
                                         new YieldingWaitStrategy());
     // define EventHandler
-    // TODO:
-
+    DemoEventHandler evHandler = new DemoEventHandler(1, latch);
+    
     // define BatchEventProcessor wrapping the EventHandler
     batchProc = new BatchEventProcessor<DemoEvent>(ringBuf,
                                                    ringBuf.newBarrier(),
                                                    evHandler);
-                                        
+    pubs = new ArrayList<DemoPublisher>();
+    final int nmsgs = NUM_MSGS_TO_PUBLISH / 3;
+    // NOTE: try it with and without pausing - w/o pauses, you'll longer stretches
+    // from the same publisher in a row
+    pubs.add( new DemoPublisher(1, nmsgs, ringBuf, cyclicBarrier).shouldPause(true) );
+    pubs.add( new DemoPublisher(2, nmsgs, ringBuf, cyclicBarrier).shouldPause(true) );
+    pubs.add( new DemoPublisher(3, nmsgs, ringBuf, cyclicBarrier).shouldPause(true) );
+
+    // set the processor sequence as the gating sequence on the RingBuffer
+    ringBuf.setGatingSequences( batchProc.getSequence() );
   }
 
   public void engage() {
-    
+    final ExecutorService execService = Executors.newFixedThreadPool(NUM_PUBLISHERS + 1);
+
+    // start a thread with the BatchEventProcessor
+    // don't need a future on this one, so call execute() instead of submit()
+    execService.execute(batchProc);
+
+    // start the publishers in separate threads
+    final List<Future<?>> futures = new ArrayList<Future<?>>();
+    for (DemoPublisher pub: pubs) {
+      futures.add( execService.submit(pub) );
+    }
+
+    try {
+      // wait for each publisher to finish (TODO: this step probably not necessary)
+      for (Future f : futures) {
+        f.get();
+      }
+
+      // wait for processor to finish 'consuming' all messages
+      latch.await();
+    } catch (Exception e) {
+      System.out.println(e.toString());
+      e.printStackTrace();
+    } finally {
+      batchProc.halt();
+      execService.shutdown();
+    }
   }
 
   public static class DemoPublisher implements Runnable {
     private final int id;
+    private final int howmany;   // how many events to publish once its run method is called
+    private boolean should_pause_b = false;
     private final RingBuffer<DemoEvent> ringBuf;
     private final CyclicBarrier cyclicBarrier;
-    private int pubcount = 0;  // number of events published
 
-    public DemoPublisher(int id, RingBuffer<DemoEvent> ringBuf) {
-      this(id, ringBuf, null);
+    public DemoPublisher(int id, int howmany, RingBuffer<DemoEvent> ringBuf) {
+      this(id, howmany, ringBuf, null);
     }
 
-    public DemoPublisher(int id, RingBuffer<DemoEvent> ringBuf, CyclicBarrier cyclicBarrier) {
+    public DemoPublisher(int id, int howmany, RingBuffer<DemoEvent> ringBuf, CyclicBarrier cyclicBarrier) {
       this.id = id;
+      this.howmany = howmany;
       this.ringBuf = ringBuf;
       this.cyclicBarrier = cyclicBarrier;
+    }
+
+    /**
+     * Whether the publisher should pause very slightly between each 
+     * event it publishes.  If yes, this can allow a more scrambled order
+     * to the published events in the RingBuffer.
+     * @return Reference to itself to allow for fluent chaining chains
+     */
+    public DemoPublisher shouldPause(boolean s) {
+      should_pause_b = s;
+      return this;
     }
 
     public void run() {
       try {
         if (cyclicBarrier != null) cyclicBarrier.await();
 
-        long seq = ringBuf.next();
-        DemoEvent ev = ringBuf.get(seq);
-        ev.setProcessId(pubcount++);
-        ev.setName("Published by DemoPublisher #" + id);
-        ringBuf.publish(seq);
+        for (int i = 0; i < howmany; i++) {
+          long seq = ringBuf.next();
+          DemoEvent ev = ringBuf.get(seq);
+          // each event will have the pubcount in processId
+          // and the name of the publisher it came from in the Name field
+          ev.setProcessId(i);
+          ev.setName("Published by DemoPublisher #" + id);
+          ringBuf.publish(seq);
+          if (should_pause_b) try { Thread.sleep(1); } catch (Exception e) {}
+        }
         
       } catch (Exception e) {
         throw new RuntimeException(e);
